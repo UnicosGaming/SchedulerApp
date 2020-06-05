cc58e9c65f98b913a7d7e2dcc88eae5c4d6c0ae2
Daniel Valcarce <davamix@gmail.com>
2020-06-05 08:40:44 +0200

    Added MsalActivity file without key

---
+++ SchedulerApp/SchedulerApp.Android/MsalActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.Identity.Client;
+using SchedulerApp.Configuration;
+
+namespace SchedulerApp.Droid
+{
+    [Activity]
+    [IntentFilter(
+        new[] { Intent.ActionView },
+        Categories = new[] { Intent.CategoryBrowsable, Intent.CategoryDefault },
+        DataHost = "auth",
+        DataScheme = Secrets.MsalID)]
+    public class MsalActivity : BrowserTabActivity
+    {
+    }
+}
